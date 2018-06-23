d1043554b9f83ff21653aea6917b22402f903838
felvct <github@felvct.com>
2018-06-23 18:27:46 +0200

    Implemented visible face tracking

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 window.onload = function() {
+  const video = document.getElementById('video');
+  const canvas = document.getElementById('canvas');
+  const context = canvas.getContext('2d');
   const tracker = new tracking.ObjectTracker('face');
   const dogSelector = document.getElementById('dog');
 
@@ -9,6 +12,7 @@ window.onload = function() {
   tracking.track('#video', tracker, { camera: true });
 
   tracker.on('track', function(event) {
+    context.clearRect(0, 0, canvas.width, canvas.height);
 
     if (!event.data.length && dogSelector.classList.contains('happy')) {
       dogSelector.classList.remove('happy');
@@ -18,6 +22,12 @@ window.onload = function() {
       if (!dogSelector.classList.contains('happy')) {
         dogSelector.classList.add('happy');
       }
+      context.strokeStyle = '#a64ceb';
+      context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+      context.font = '11px Helvetica';
+      context.fillStyle = "#fff";
+      context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
+      context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
     });
   });
 };
